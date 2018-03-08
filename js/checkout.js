fd528574b755f0a4ff67f88d199cbb1aa9924015
G4br13l4 <gabolf93@gmail.com>
2018-03-08 13:43:01 -0600

    local storage: funcion de calcular suma

--- js/checkout.js
+++ js/checkout.js
@@ -1,5 +1,24 @@
-function calculateTotal() {
-  //como le hacemos para extraer toda
-  //de cantidades de los elementos
-  //en mi carrito
+const productsToBuy = JSON.parse(localStorage.getItem("productsInCart"));
+let productsInCartContainer = document.getElementById("products-container");
+let sumOfProducts = 0; 
+
+function calculateTotal(productsToBuy) {
+  
+  productsToBuy.forEach(element => {
+    const nameOfProduct =element.title;
+    const priceOfProduct = element.price;
+    sumOfProducts += priceOfProduct;
+    var newProductinCart = `
+      <th scope="row">${nameOfProduct}</th>
+      <td>${priceOfProduct}</td>
+    `
+    let productContainer = document.createElement("TR");
+    productContainer.innerHTML = newProductinCart;
+    productsInCartContainer.prepend(productContainer);
+  });
+  const sumContainer = document.getElementById("sum-of-products");
+  sumContainer.innerText = sumOfProducts;
 }
+
+calculateTotal(productsToBuy);
+
